136dd6f6a9825ec541c21f0e7eb0e61556d27c35
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2022-03-06 09:52:18 +0300

    Initial Commit

---
+++ Expense Tracker App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expense Tracker App
+//
+//  Created by admin1 on 06.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
