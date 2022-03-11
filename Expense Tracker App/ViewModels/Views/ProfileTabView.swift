c4e5cb0ead0a4a105da9ce72e8dc0f87e63fb4ad
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2022-03-11 12:13:23 +0300

    Start of the Profile page execution

--- Expense Tracker App/ViewModels/Views/ProfileTabView.swift
+++ Expense Tracker App/ViewModels/Views/ProfileTabView.swift
@@ -10,8 +10,51 @@ import UIKit
 class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        initialize()
 //        title = "Profile"
         
 }
+    
+    private func initialize() {
+        
+//        let imageView: UIImageView {
+           let imageHeader = UIImageView(image: #imageLiteral(resourceName: "Rectangle 9.png"))
+            //image.backgroundColor = .lightGray
+//            image.contentMode = .scaleAspectFill
+//            image.layer.cornerRadius = 2
+//            image.clipsToBounds = true
+           
+
+        view.addSubview(imageHeader)
+        imageHeader.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.equalToSuperview()
+            make.trailing.equalToSuperview()
+            //make.left.equalToSuperview().inset(0)
+            //make.top.equalToSuperview().inset(0)
+        }
+        
+        let imageRings = UIImageView(image: #imageLiteral(resourceName: "Group 6.1"))
+        view.addSubview(imageRings)
+        imageRings.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.equalToSuperview()
+            make.left.equalToSuperview().inset(0)
+            make.top.equalToSuperview().inset(0)
+        }
+        
+        let buttonBell = UIButton(type: .custom)
+        buttonBell.setImage(UIImage(named: "Frame4z"), for: .normal)
+        view.addSubview(buttonBell)
+        buttonBell.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(78)
+            make.trailing.equalToSuperview()
+            make.leading.equalToSuperview().inset(350)
+        }
+        buttonBell.addTarget(self, action: #selector(buttonBellTapped), for: .touchUpInside)
+    }
+
+@objc private func buttonBellTapped() {
+    print("We want to get a username")
 }
+    }
--- Expense Tracker App/ViewModels/Views/StatisticTabView.swift
+++ Expense Tracker App/ViewModels/Views/StatisticTabView.swift
@@ -6,13 +6,52 @@
 //
 
 import UIKit
+import SnapKit
 
 class StatisticViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGray
+        initialize()
 //        title = "Statistic"
-       
+    }
         
-}
+        private func initialize() {
+            view.backgroundColor = UIColor(red: 241/255, green: 238/255, blue: 228/255, alpha: 1)
+            
+            let label = UILabel()
+            label.text = "✋ Welcome!"
+            label.font = UIFont.systemFont(ofSize: 20)
+            view.addSubview(label)
+            label.snp.makeConstraints { make in
+                make.left.equalToSuperview().inset(50)
+                make.top.equalToSuperview().inset(150)
+            }
+            
+            let extraLabel = UILabel()
+            extraLabel.text = "Extra text Extra text Extra text Extra text Extra text Extra text Extra text Extra text Extra text Extra text v Extra text Extra text Extra text Extra text"
+            extraLabel.numberOfLines = 0
+            view.addSubview(extraLabel)
+            extraLabel.snp.makeConstraints { make in
+                make.left.right.equalToSuperview().inset(50)
+                make.top.equalTo(label).inset(50)
+            }
+            
+            let button = UIButton(type: .system)
+            button.backgroundColor = UIColor(red: 84/255, green: 118/255, blue: 171/255, alpha: 1)
+            button.setTitleColor(.white, for: .normal)
+            button.layer.cornerRadius = 20
+            button.setTitle("Get your username ->", for: .normal)
+            view.addSubview(button)
+            button.snp.makeConstraints { make in
+                make.centerX.equalToSuperview()
+                make.width.equalTo(250)
+                make.bottom.equalToSuperview().inset(150)
+                make.height.equalTo(40)
+            }
+            button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        }
+    
+    @objc private func buttonTapped() {
+        print("We want to get a username")
+    }
 }
